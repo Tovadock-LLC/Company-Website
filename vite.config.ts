29a46f3fcfab5ec1fd0437992e1128b33f61af9f
Jim Knudstrup <jim.knudstrup@gmail.com>
2025-07-11 21:26:27 -0700

    Replace vite-bundle-analyzer with rollup-plugin-visualizer
    vite-bundle-analyzer was kinda crap

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from '@tailwindcss/vite';
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    visualizer({template: "treemap"})
   ],
   resolve: {
     alias: {
