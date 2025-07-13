f37897a728e897e916abb552de42fc96091e3d00
Jim Knudstrup <jim.knudstrup@gmail.com>
2025-07-13 11:46:28 -0700

    Add svgr

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
@@ -9,6 +10,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    svgr(),
     visualizer({template: "treemap"})
   ],
   resolve: {
