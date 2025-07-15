885e500bd696632479ec176cc84c6b5758c756a4
Jim Knudstrup <jim.knudstrup@gmail.com>
2025-07-15 08:43:49 -0700

    Init Services component

---
+++ src/elements/hero/Services.tsx
@@ -0,0 +1,23 @@
+import { type FC } from "react";
+
+import { ContentWrapper } from "@/components/helpers/ContentWrapper";
+
+const Service: FC = () => {
+  return (
+    <ContentWrapper>
+      <div></div>
+    </ContentWrapper>
+  );
+};
+
+export const Services: FC = () => {
+  return (
+    <>
+      <>
+        <div className="flex justify-center">
+          <h1>Our Services</h1>
+        </div>
+      </>
+    </>
+  );
+};
