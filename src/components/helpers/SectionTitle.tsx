d3c34aaeb6bf5891cc93913d2ba9d9af36de19f8
Jim Knudstrup <jim.knudstrup@gmail.com>
2025-07-16 12:43:16 -0700

    Rewrite SectionTitle with proper variants

--- src/components/helpers/SectionTitle.tsx
+++ src/components/helpers/SectionTitle.tsx
@@ -1,18 +1,38 @@
-import type { FC, PropsWithChildren } from "react";
+import type { FC, HTMLAttributes } from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+import { clsx } from "clsx";
 
-type SectionTitleProps = {
-  textColor?: "light" | "dark";
-} & PropsWithChildren;
+import { cn } from "@/lib/utils";
+
+const sectionTitleVariants = cva(clsx`py-8 text-5xl font-bold`, {
+  variants: {
+    textColor: {
+      light: "text-white",
+      dark: "text-primary",
+    },
+  },
+  defaultVariants: {
+    textColor: "dark",
+  },
+});
+
+type SectionTitleProps = HTMLAttributes<HTMLHeadingElement> &
+  VariantProps<typeof sectionTitleVariants>;
 
 export const SectionTitle: FC<SectionTitleProps> = ({
+  className,
   textColor,
   children,
+  ...props
 }) => {
-  const color = textColor === "light" ? "text-white" : "text-primary";
-
   return (
     <div className="flex w-full justify-center">
-      <h1 className={`${color} py-8 text-5xl font-bold`}>{children}</h1>
+      <h1
+        className={cn(sectionTitleVariants({ textColor, className }))}
+        {...props}
+      >
+        {children}
+      </h1>
     </div>
   );
 };
